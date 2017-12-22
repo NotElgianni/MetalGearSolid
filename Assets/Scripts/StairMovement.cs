2d731efa1c1af087e7c05a15d8655c015ffe80b5
ElgianniKolf <elgianni14@hotmail.com>
2017-12-22 23:13:45 +0100

    Added my own scripts

---
+++ Assets/Scripts/StairMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StairMovement : MonoBehaviour {
+
+    private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.transform.tag == "stair")
+        {
+           
+        }
+        else
+        {
+            Physics.gravity = new Vector3(0, -10f * 10f, 0);
+            Debug.Log("touching stairs");
+        }
+    }
+}
